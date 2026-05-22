9a084dd2c5501fdbd57383adee27f772438078a5
daizeuz-dred <daizeuzdred@gmail.com>
2026-05-22 12:43:40 +0800

    Change to Hyperion
    Signed-off-by: daizeuz-dred <daizeuzdred@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=InfixKernel-spaced for Realme Narzo50/8i(spaced)
+kernel.string=HYPERION-spaced for Realme Narzo50/8i(spaced)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
