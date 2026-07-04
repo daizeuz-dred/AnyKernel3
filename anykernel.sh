2b7baa638008a247d52efbbffa44303390955a85
daizeuz-dred <daizeuzdred@gmail.com>
2026-07-04 09:35:53 +0800

    Update for APEX v2 KSUN v3.2.0 & SUSFS v2.2.0
    Signed-off-by: daizeuz-dred <daizeuzdred@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=APEX-Kernel-spaced for Realme Narzo50/8i(spaced)
+kernel.string=APEX-Kernel-v2-spaced for Realme Narzo50/8i(spaced)
 do.devicecheck=1
 do.modules=0
 do.systemless=1
